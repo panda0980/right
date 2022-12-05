2ac1ec61cabb4b7dd9dff75f7124f5ccfacf1fdf
DOBBY897 <100317014+DOBBY897@users.noreply.github.com>
2022-12-05 18:43:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5614754139:AAHic-ZQLWl5lt5SXF4MJLMY7IZXCp9xvzs")
-API_ID = int(os.environ.get("API_ID", "11671320"))
-API_HASH = os.environ.get("API_HASH", "8e409e260f1d80f0ead65da912ee07bb")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5784723595:AAEL26cA8Qbj-UcDeFoTquL3H-5bzrPxmAU")
+API_ID = int(os.environ.get("API_ID", "23217719"))
+API_HASH = os.environ.get("API_HASH", "03b3a05a0cf5601241463840ba9900d0")
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1983530070").split())
-DATA_URL = os.environ.get("DATA_URL", "mongodb+srv://panda:eGOv61gScegMQddj@cluster0.lilgk.mongodb.net/?retryWrites=true&w=majority")
+DATA_URL = os.environ.get("DATA_URL", "mongodb+srv://SAM:Flo@1303@cluster0.5nyz2uw.mongodb.net/?retryWrites=true&w=majority")
 DATA_NAME = os.environ.get("DATA_NAME", "Music_downloder")
