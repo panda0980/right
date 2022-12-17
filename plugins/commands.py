daee89dc7d9532f08ecf5111802956e575202594
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-18 00:17:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -209,7 +209,7 @@ async def broadcast(bot:Client, message):
                 if sent % 25 == 0:
                     await asyncio.sleep(1)
                 try:
-                    await bot.send_message(chat["id"], text=text)
+                    await message.copy(chat["id"], text=text)
                     sent += 1
                 except (PeerIdInvalid, ChannelInvalid):
                     failed += 1
